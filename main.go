cc3a38bc5e350cdacbbc88a5e8f709623c8c21de
Joao Ferreira <jferreira.ba@gmail.com>
2019-07-25 01:30:12 -0300

    [+] Simples app web with golang

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.ParseFiles("templates/index.html"))
+
+		if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":3000", nil))
+}
